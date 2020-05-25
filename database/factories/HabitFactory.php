a11cc75ec027fd3cd04a25bf4e79e2c025bdd4f0
orbijo <arkorbz13@gmail.com>
2020-05-25 11:31:26 +0800

    added migrations, seeder, factory, controllers, and models with relation

---
+++ database/factories/HabitFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Habit;
+use Faker\Generator as Faker;
+
+$factory->define(Habit::class, function (Faker $faker) {
+    $ratings = $faker->randomElement(['far fa-frown', 'far fa-meh', 'far fa-smile']);
+    return [
+        'description' => $faker->text($maxNbChars = 30),
+        'reason' => $faker->text($maxNbChars = 140),
+        'rating' => $ratings,
+    ];
+});
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,19 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $gender = $faker->randomElement(['male', 'female']);
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->firstName($gender),
+        'last_name' => $faker->lastName(),
+        'birthday' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'sex' => $gender,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
+});
+
+$factory->afterCreating(User::class, function ($user, Faker $faker) {
+   $user->habits()->save(factory(App\Habit::class)->make());
 });
